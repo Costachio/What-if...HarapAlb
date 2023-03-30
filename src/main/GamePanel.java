9049848dd0227b26346f4eaa0093994022b39d85
Costache Andrei <andrei.costache@feel-it-services.com>
2023-03-30 15:29:48 +0300

    -fps

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -9,8 +9,10 @@ import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
     private MouseInputs mouseInputs;
-    private int xDelta = 100;
-    private int yDelta = 100;
+    private int xDelta = 100, yDelta = 100;
+    private int frames = 0;
+    private long lastCheck = 0;
+
 
     public GamePanel() {
         mouseInputs = new MouseInputs(this);
@@ -22,20 +24,20 @@ public class GamePanel extends JPanel {
 
     public void changeXDelta(int value) {
         this.xDelta += value;
-        repaint();
+
 
     }
 
     public void changeYDelta(int value) {
         this.yDelta += value;
-        repaint();
+
 
     }
 
     public void setRectPos(int x, int y) {
         this.xDelta = x;
         this.yDelta = y;
-        repaint();
+
     }
 
 
@@ -43,7 +45,13 @@ public class GamePanel extends JPanel {
         super.paintComponent(g);
 
         g.fillRect(xDelta, yDelta, 200, 50);
-
+        frames++;
+        if (System.currentTimeMillis() - lastCheck >= 1000) {
+            lastCheck = System.currentTimeMillis();
+            System.out.println("FPS: " + frames);
+            frames = 0;
+        }
+        repaint();
     }
 
 }
