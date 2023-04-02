4424e22fa8745df45af85e517fa42084f70b33bc
Costache Andrei <andrei.costache@feel-it-services.com>
2023-04-02 19:19:05 +0300

    -animations

--- src/inputs/MouseInputs.java
+++ src/inputs/MouseInputs.java
@@ -1,50 +1,57 @@
 package inputs;
 
-import main.GamePanel;
-
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
+import main.GamePanel;
+
 public class MouseInputs implements MouseListener, MouseMotionListener {
-    private GamePanel gamePanel;
-    public MouseInputs(GamePanel gamePanel)
-    {
-        this.gamePanel=gamePanel;
-    }
-    @Override
-    public void mouseClicked(MouseEvent mouseEvent) {
-        System.out.println("Mouse clicked");
 
-    }
+	private GamePanel gamePanel;
 
-    @Override
-    public void mousePressed(MouseEvent mouseEvent) {
+	public MouseInputs(GamePanel gamePanel) {
+		this.gamePanel = gamePanel;
+	}
 
-    }
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
 
-    @Override
-    public void mouseReleased(MouseEvent mouseEvent) {
+	}
 
-    }
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		gamePanel.setRectPos(e.getX(), e.getY());
+	}
 
-    @Override
-    public void mouseEntered(MouseEvent mouseEvent) {
+	@Override
+	public void mouseClicked(MouseEvent e) {
 
-    }
+	}
 
-    @Override
-    public void mouseExited(MouseEvent mouseEvent) {
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
 
-    }
+	}
 
-    @Override
-    public void mouseDragged(MouseEvent mouseEvent) {
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
 
-    }
+	}
 
-    @Override
-    public void mouseMoved(MouseEvent e) {
-        gamePanel.setRectPos(e.getX(), e.getY());
-    }
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
 }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,57 +1,96 @@
 package main;
 
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
 import inputs.KeyboardInputs;
 import inputs.MouseInputs;
 
-import java.awt.Graphics;
-
-import javax.swing.JPanel;
-
 public class GamePanel extends JPanel {
     private MouseInputs mouseInputs;
-    private int xDelta = 100, yDelta = 100;
-    private int frames = 0;
-    private long lastCheck = 0;
+    private float xDelta = 100, yDelta = 100;
+    private BufferedImage img;
+    private BufferedImage [] idleAnimation;
+    private  int animationTick, animationIndex, animationSpeed = 15;
 
 
     public GamePanel() {
         mouseInputs = new MouseInputs(this);
+
+
+        importImg();
+        loadAnimations();
+
+        setPanelSize();
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
+    }
 
+    private void loadAnimations() {
+        idleAnimation = new BufferedImage[12];
+        for( int i = 0; i < idleAnimation.length; i++)
+            idleAnimation[i]=img.getSubimage(i*32,0,32, 32);
+
+    }
+
+	private void importImg() {
+		InputStream is = getClass().getResourceAsStream("/player_sprites.png");
+		try {
+			img = ImageIO.read(is);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				is.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+    private void setPanelSize() {
+        Dimension size = new Dimension(1280, 800);
+        setPreferredSize(size);
     }
 
     public void changeXDelta(int value) {
         this.xDelta += value;
-
-
     }
 
     public void changeYDelta(int value) {
         this.yDelta += value;
-
-
     }
 
     public void setRectPos(int x, int y) {
         this.xDelta = x;
         this.yDelta = y;
+    }
+    private void updateAnimationTick() {
+        animationTick++;
+        if(animationTick >= animationSpeed)
+        {
+            animationTick =0;
+            animationIndex++;
+            if(animationTick == idleAnimation.length)
+                animationIndex = 0;
+        }
 
     }
-
 
     public void paintComponent(Graphics g) {
-        super.paintComponent(g);
+        updateAnimationTick();
 
-        g.fillRect(xDelta, yDelta, 200, 50);
-        frames++;
-        if (System.currentTimeMillis() - lastCheck >= 1000) {
-            lastCheck = System.currentTimeMillis();
-            System.out.println("FPS: " + frames);
-            frames = 0;
-        }
-        repaint();
+        g.drawImage(idleAnimation[animationIndex], (int)xDelta, (int)yDelta,64, 64, null);
+
     }
+
+
 
 }
